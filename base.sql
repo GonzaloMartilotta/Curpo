d3987b189680952845b90040e2d4990f6e88eb18
Gonzalo Martilotta <Gonzalomartilotta@gmail.com>
2026-09-04 12:11:07 -0300

    Define the Data Base structure

--- base.sql
+++ base.sql
@@ -1,15 +1,31 @@
 CREATE TABLE users(
     id SERIAL PRIMARY KEY,
-    name TEXT,
-    mail TEXT,
-    phone INT
+    name VARCHAR NOT NULL,
+    mail VARCHAR NOT NULL UNIQUE,
+    phone VARCHAR
 );
 
 CREATE TABLE vehicles(
     id SERIAL PRIMARY KEY,
-    brand TEXT,
-    model TEXT,
-    price REAL,
-    seller_id INT REFERENCES users(id)
+    brand VARCHAR NOT NULL,
+    model VARCHAR,
+    used BOOLEAN NOT NULL,
+    km INT NOT NULL CHECK (km >= 0),
+    specs JSON
 );
 
+CREATE TABLE images(
+    id SERIAL PRIMARY KEY,
+    url VARCHAR NOT NULL UNIQUE,
+    vehicle_id int REFERENCES vehicles(id)
+);
+
+CREATE TABLE post(
+    id SERIAL PRIMARY KEY,
+    url VARCHAR NOT NULL UNIQUE,
+    price INT NOT NULL CHECK (price > 0),
+    vehicle_id INT REFERENCES vehicles(id),
+    create_time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    state SMALLINT NOT NULL CHECK (state BETWEEN 0 AND 3),
+    seller_id INT REFERENCES users(id)
+);
